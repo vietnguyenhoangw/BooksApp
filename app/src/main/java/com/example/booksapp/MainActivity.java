0f39b721ae23f1595aa7613aadbebad027606405
vietnguyenhoangw <vietnguyenhoangw@gmail.com>
2019-10-05 13:23:57 +0700

    [UPD] - :sparkles: add new refresh data and delete book item

--- app/src/main/java/com/example/booksapp/MainActivity.java
+++ app/src/main/java/com/example/booksapp/MainActivity.java
@@ -1,22 +1,33 @@
 package com.example.booksapp;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
+import android.widget.Toolbar;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -26,7 +37,9 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<Book> arrayList;
     BookListAdapter bookListAdapter;
 
-    String url = "http://192.168.1.10:5000/books";
+    String url = "http://192.168.1.12:5000/books";
+
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,6 +68,51 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        registerForContextMenu(listView);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.actionbar_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.item_menu, menu);
+
+        super.onCreateContextMenu(menu, v, menuInfo);
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        int index = info.position;
+
+        Book book = arrayList.get(index);
+
+        switch (item.getItemId()) {
+            case R.id.item_delete:
+                delete(book.getBookID());
+                Toast.makeText(this, "Deleted", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onContextItemSelected(item);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()) {
+            case R.id.reset:
+                Toast.makeText(this, "Refeshed", Toast.LENGTH_SHORT).show();
+                arrayList.clear();
+                getBooks();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     // get book from API using volley
@@ -80,5 +138,31 @@ public class MainActivity extends AppCompatActivity {
         });
 
         requestQueue.add(stringRequest);
+    }
+
+    private void delete(int id) {
+        RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
+
+        try {
+            JSONObject jsonRequest = new JSONObject();
+
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.DELETE,
+                    url + "/" + id, null, new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+//                    Toast.makeText(MainActivity.this, response.toString(), Toast.LENGTH_SHORT).show();
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+
+                }
+            });
+
+            requestQueue.add(jsonObjectRequest);
+        }
+        catch (Exception e) {
+
+        }
     }
 }
